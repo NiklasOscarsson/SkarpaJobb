57c3d28dd889cd8599fe866e5c52973a3fb4c0c6
Niklas <Niklas.Oscarsson@ga.ntig.se>
2021-06-23 16:43:12 +0200

    fuckYea

---
+++ src/pages/student.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="back">
+    <div class="div-size">
+      <img class="img1and3" src="" alt="">
+    </div>
+    <div class="div-size"></div>
+    <div class="div-size"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .back{
+    height: 100vh;
+  }
+  .div-size{
+    height: 33.14vh;
+    border: black 1px solid;
+  }
+  .img1and3{
+    height: 100%;
+    position:relative;
+    right: 5px;
+  }
+</style>
